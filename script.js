d472ef7d3d1f9f104c8a7014bf53b8080df809fa
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-02 09:55:40 +0300

    Created the parallax backgrounds

--- script.js
+++ script.js
@@ -1,98 +1,66 @@
-const animationState = [
-  {
-    name: 'idle',
-    frames: 7,
-  },
-  {
-    name: 'jump',
-    frames: 7,
-  },
-  {
-    name: 'fall',
-    frames: 7,
-  },
-  {
-    name: 'run',
-    frames: 9,
-  },
-  {
-    name: 'dizzy',
-    frames: 11,
-  },
-  {
-    name: 'sit',
-    frames: 5,
-  },
-  {
-    name: 'roll',
-    frames: 7,
-  },
-  {
-    name: 'bite',
-    frames: 7,
-  },
-  {
-    name: 'ko',
-    frames: 12,
-  },
-  {
-    name: 'getHit',
-    frames: 4,
-  },
-];
-
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 500);
+let gameSpeed = 5;
 
-let playerState = 'idle';
-const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change', (ev) => (playerState = ev.target.value));
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'images/layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'images/layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'images/layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'images/layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'images/layer-5.png';
 
-const playerImage = new Image();
-playerImage.src = 'images/shadow_dog.png';
-const destinationX = 0;
-const destinationY = 0;
-const spriteWidth = 575; //6876px/12column = 573px per 1column
-const spriteHeight = 523; //5230px/10SpriteColumns = 523px per 1row
-let gameFrame = 0;
-const everyCountOfFrames = 5;
+window.addEventListener('load', () => {
+  const slider = document.getElementById('slider');
+  slider.value = gameSpeed;
+  const showGameSpeed = document.getElementById('showGameSpeed');
+  showGameSpeed.innerText = gameSpeed;
+  slider.addEventListener('change', (ev) => {
+    gameSpeed = ev.target.value;
+    showGameSpeed.innerText = gameSpeed;
+  });
 
-const spriteAnimations = [];
-
-animationState.forEach((state, idx) => {
-  let frames = {
-    loc: [],
-  };
-
-  for (let j = 0; j < state.frames; j++) {
-    let positionX = j * spriteWidth;
-    let positionY = idx * spriteHeight;
-    frames.loc.push({ x: positionX, y: positionY });
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 1667;
+      this.height = 500;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      if (this.x <= -this.width) this.x = 0;
+      this.x = Math.floor(this.x - this.speed);
+    }
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
   }
-  spriteAnimations[state.name] = frames;
+
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
+
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+  const animate = () => {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach((object) => {
+      object.update();
+      object.draw();
+    });
+    requestAnimationFrame(animate);
+  };
+  animate();
 });
-
-const animate = () => {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position =
-    Math.floor(gameFrame / everyCountOfFrames) % spriteAnimations[playerState].loc.length;
-  let frameX = spriteWidth * position;
-  let frameY = spriteAnimations[playerState].loc[position].y;
-  ctx.drawImage(
-    playerImage,
-    frameX,
-    frameY,
-    spriteWidth,
-    spriteHeight,
-    destinationX,
-    destinationY,
-    spriteWidth,
-    spriteHeight
-  );
-
-  gameFrame++;
-  requestAnimationFrame(animate);
-};
-animate();
