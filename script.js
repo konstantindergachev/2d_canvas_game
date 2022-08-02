6de57c3fae6d0c33d8812509cb8c813263a94552
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-02 15:33:18 +0300

    Created the 1 step of NPC movement

--- script.js
+++ script.js
@@ -7,20 +7,43 @@ const CANVAS_HEIGHT = (canvas.height = 900);
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
+let gameFrame = 0;
+
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
-    this.speed = Math.random() * 4 - 2;
+    this.image = new Image();
+    this.image.src = 'images/enemy_bat_1.png';
+    // this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 83;
+    this.spriteHeight = 44;
+    this.width = this.spriteWidth / 0.75;
+    this.height = this.spriteHeight / 0.75;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
+
+    //animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 for (let i = 0; i < numberOfEnemies; i++) {
@@ -33,6 +56,7 @@ window.addEventListener('load', () => {
       enemy.update();
       enemy.draw();
     });
+    gameFrame++;
     requestAnimationFrame(animate);
   };
   animate();
