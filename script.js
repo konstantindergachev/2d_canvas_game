a0ee239618d97ff606657e2979e7e6cb5ed088a2
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-02 15:48:04 +0300

    Created the 2 step of NPC movement

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 900);
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -12,22 +12,26 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = 'images/enemy_bat_1.png';
-    // this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 83;
-    this.spriteHeight = 44;
-    this.width = this.spriteWidth / 0.75;
-    this.height = this.spriteHeight / 0.75;
+    this.image.src = 'images/enemy_bat_3.png';
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = Math.random() * 2;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 7;
   }
   update() {
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
-
+    this.x -= this.speed;
+    this.y += this.curve * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
     //animate sprites
+    if (this.x + this.width < 0) this.x = canvas.width;
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
