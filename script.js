9b91f07f6b9b7ffb034cce4017912fed24967c17
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-02 10:32:56 +0300

    Created the base NPC movement

--- script.js
+++ script.js
@@ -1,64 +1,37 @@
+/**@type {HTMLCanvasElement} */
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = (canvas.width = 800);
-const CANVAS_HEIGHT = (canvas.height = 500);
-let gameSpeed = 5;
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 900);
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
-const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'images/layer-1.png';
-const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'images/layer-2.png';
-const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'images/layer-3.png';
-const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'images/layer-4.png';
-const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'images/layer-5.png';
-
-window.addEventListener('load', () => {
-  const slider = document.getElementById('slider');
-  slider.value = gameSpeed;
-  const showGameSpeed = document.getElementById('showGameSpeed');
-  showGameSpeed.innerText = gameSpeed;
-  slider.addEventListener('change', (ev) => {
-    gameSpeed = ev.target.value;
-    showGameSpeed.innerText = gameSpeed;
-  });
-
-  class Layer {
-    constructor(image, speedModifier) {
-      this.x = 0;
-      this.y = 0;
-      this.width = 1667;
-      this.height = 500;
-      this.image = image;
-      this.speedModifier = speedModifier;
-      this.speed = gameSpeed * this.speedModifier;
-    }
-    update() {
-      this.speed = gameSpeed * this.speedModifier;
-      if (this.x <= -this.width) this.x = 0;
-      this.x = Math.floor(this.x - this.speed);
-    }
-    draw() {
-      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-    }
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+    this.speed = Math.random() * 4 - 2;
   }
-
-  const layer1 = new Layer(backgroundLayer1, 0.2);
-  const layer2 = new Layer(backgroundLayer2, 0.4);
-  const layer3 = new Layer(backgroundLayer3, 0.6);
-  const layer4 = new Layer(backgroundLayer4, 0.8);
-  const layer5 = new Layer(backgroundLayer5, 1);
-
-  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
+  update() {
+    this.x += this.speed;
+    this.y += this.speed;
+  }
+  draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
+window.addEventListener('load', () => {
   const animate = () => {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    gameObjects.forEach((object) => {
-      object.update();
-      object.draw();
+    enemiesArray.forEach((enemy) => {
+      enemy.update();
+      enemy.draw();
     });
     requestAnimationFrame(animate);
   };
