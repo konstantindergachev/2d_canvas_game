86679ca14150f798c55bdc2ac5c46800eaaa962e
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-08 17:46:00 +0300

    Worked with frames

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
   const ctx = canvas.getContext('2d');
   canvas.width = 800;
   canvas.height = 720;
+  let enemies = [];
 
   class InputHandler {
     constructor() {
@@ -43,13 +44,15 @@ window.addEventListener('load', () => {
       this.image = document.getElementById('playerImage');
       this.frameX = 0;
       this.frameY = 0;
+      this.maxFrame = 8;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
       this.speed = 0;
       this.velocityY = 0;
       this.weight = 1;
     }
     draw(contex) {
-      contex.fillStyle = 'white';
-      contex.fillRect(this.x, this.y, this.width, this.height);
       contex.drawImage(
         this.image,
         this.frameX * this.width,
@@ -62,7 +65,17 @@ window.addEventListener('load', () => {
         this.height
       );
     }
-    update(input) {
+    update(input, deltaTime) {
+      //sprite animation
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) this.frameX = 0;
+        else this.frameX++;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+
+      //controls
       if (input.keys.indexOf('ArrowRight') > -1) {
         this.speed = 5;
       } else if (input.keys.indexOf('ArrowLeft') > -1) {
@@ -82,9 +95,11 @@ window.addEventListener('load', () => {
       this.y += this.velocityY;
       if (!this.#onGround()) {
         this.velocityY += this.weight;
+        this.maxFrame = 5;
         this.frameY = 1;
       } else {
         this.velocityY = 0;
+        this.maxFrame = 8;
         this.frameY = 0;
       }
       if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
@@ -129,6 +144,12 @@ window.addEventListener('load', () => {
       this.x = this.gameWidth;
       this.y = this.gameHeight - this.height;
       this.frameX = 0;
+      this.maxFrame = 5;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
+      this.speed = 8;
+      this.markForDeletion = false;
     }
     draw(context) {
       context.drawImage(
@@ -143,28 +164,53 @@ window.addEventListener('load', () => {
         this.height
       );
     }
-    update() {
-      this.x--;
+    update(deltaTime) {
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) this.frameX = 0;
+        else this.frameX++;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) this.markForDeletion = true;
     }
   }
 
-  const handleEnemies = () => {};
+  const handleEnemies = (deltaTime) => {
+    if (enemyTimer > enemyInterval + randomEnemyInterval) {
+      enemies.push(new Enemy(canvas.width, canvas.height));
+      enemyTimer = 0;
+    } else {
+      enemyTimer += deltaTime;
+    }
+    enemies.forEach((enemy) => {
+      enemy.draw(ctx);
+      enemy.update(deltaTime);
+    });
+    enemies = enemies.filter((enemy) => !enemy.markForDeletion);
+  };
   const displayStatusText = () => {};
 
   const input = new InputHandler();
   const player = new Player(canvas.width, canvas.height);
   const background = new Background(canvas.width, canvas.height);
-  const enemy1 = new Enemy(canvas.width, canvas.height);
 
-  const animate = () => {
+  let lastTime = 0;
+  let enemyTimer = 0;
+  let enemyInterval = 1000;
+  let randomEnemyInterval = Math.random() * 1000 + 500;
+
+  const animate = (timeStamp) => {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
     // background.update();
     player.draw(ctx);
-    player.update(input);
-    enemy1.draw(ctx);
-    enemy1.update();
+    player.update(input, deltaTime);
+    handleEnemies(deltaTime);
     requestAnimationFrame(animate);
   };
-  animate();
+  animate(0);
 });
