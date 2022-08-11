3d918fde4ddd62dce587aa8d6db93e7348baa521
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-11 10:05:37 +0300

    Created the player jumping states

--- js/player.js
+++ js/player.js
@@ -5,6 +5,10 @@ import {
   SittingRight,
   RunningLeft,
   RunningRight,
+  JumpingLeft,
+  JumpingRight,
+  FallingLeft,
+  FallingRight,
 } from './state.js';
 
 export default class Player {
@@ -18,6 +22,10 @@ export default class Player {
       new SittingRight(this),
       new RunningLeft(this),
       new RunningRight(this),
+      new JumpingLeft(this),
+      new JumpingRight(this),
+      new FallingLeft(this),
+      new FallingRight(this),
     ];
     this.currentState = this.states[1];
     this.image = document.getElementById('dogImage');
@@ -25,6 +33,10 @@ export default class Player {
     this.height = 181.83;
     this.x = this.gameWidth / 2 - this.width / 2;
     this.y = this.gameHeight - this.height;
+
+    this.velocityY = 0;
+    this.weight = 1;
+
     this.frameX = 0;
     this.frameY = 0;
     this.speed = 0;
@@ -49,9 +61,20 @@ export default class Player {
     this.x += this.speed;
     if (this.x <= 0) this.x = 0;
     else if (this.x >= this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+    //vertical movement
+    this.y += this.velocityY;
+    if (!this.onGround()) {
+      this.velocityY += this.weight;
+    } else {
+      this.velocityY = 0;
+    }
+    if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
   }
   setState(state) {
     this.currentState = this.states[state];
     this.currentState.enter();
+  }
+  onGround() {
+    return this.y >= this.gameHeight - this.height;
   }
 }
