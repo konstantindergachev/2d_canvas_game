a900fcc65ecce6aee8a808e6bc80f7ea4783093b
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-12 10:37:02 +0300

    Added the different enemies

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { Background } from './js/background.js';
+import { ClimbingEnemy, FlyingEnemy, GroundEnemy } from './js/enemies.js';
 import { InputHandler } from './js/input.js';
 import { Player } from './js/player.js';
 
@@ -18,14 +19,39 @@ window.addEventListener('load', function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+
+      //handleEnemies
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+        if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      });
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+    }
+    addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+      this.enemies.push(new FlyingEnemy(this));
     }
   }
 
