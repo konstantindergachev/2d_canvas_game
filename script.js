631c2075238a71a5a7585dc159a0c59679558236
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-08-12 13:49:22 +0300

    Created a base collision

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { Background } from './js/background.js';
 import { ClimbingEnemy, FlyingEnemy, GroundEnemy } from './js/enemies.js';
 import { InputHandler } from './js/input.js';
 import { Player } from './js/player.js';
+import { UI } from './js/UI.js';
 
 window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
@@ -19,10 +20,13 @@ window.addEventListener('load', function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      this.UI = new UI(this);
       this.enemies = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.debug = true;
+      this.score = 0;
+      this.fontColor = 'black';
     }
     update(deltaTime) {
       this.background.update();
@@ -48,6 +52,8 @@ window.addEventListener('load', function () {
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+
+      this.UI.draw(context);
     }
     addEnemy() {
       if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
